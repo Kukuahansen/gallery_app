a348ba37363fc386bff4f16a0ba11200a627cbed
Kukuahansen <augustinahanson@gmail.com>
2021-09-17 16:47:53 +0100

    final gallery app project

---
+++ lib/userData.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kukua Hansen",
+    "userLocation": "Mankessim ",
+    "userProfileImage": "images/profile_1.jpeg",
+    "userPost": "images/picture_1.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Tina Baker",
+    "userLocation": "Akyempim",
+    "userProfileImage": "images/profile_2.jfif",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kojo Antwi",
+    "userLocation": "Obuasi",
+    "userProfileImage": "images/profile_3.jpg",
+    "userPost": "images/picture_3.jfif",
+    "userPostLikes": "23",
+  }
+];
